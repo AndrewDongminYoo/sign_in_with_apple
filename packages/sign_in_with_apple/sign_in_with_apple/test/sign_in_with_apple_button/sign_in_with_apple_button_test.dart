cc2d402d67423ba9d11f335d8eac4a6e144b02ae
Dongmin Yu <ydm2790@gmail.com>
2025-01-27 21:22:00 +0900

    chore: 🎉 add pubspec overrides for sign_in_with_apple packages and update dependencies

--- packages/sign_in_with_apple/sign_in_with_apple/test/sign_in_with_apple_button/sign_in_with_apple_button_test.dart
+++ packages/sign_in_with_apple/sign_in_with_apple/test/sign_in_with_apple_button/sign_in_with_apple_button_test.dart
@@ -3,7 +3,7 @@
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide IconAlignment;
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sign_in_with_apple/src/widgets/sign_in_with_apple_button.dart';
